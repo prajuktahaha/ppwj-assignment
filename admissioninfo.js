c69a72a4c39dd1583139d50d40b2f796ad50b380
Prajukta <prajuktaha@gmail.com>
2025-09-16 02:13:52 +0530

    online admission form

---
+++ admissioninfo.js
@@ -0,0 +1,6 @@
+let fullname = prompt("Enter your fullname");
+var age = prompt("Enter your current age");
+const pi = 3.14159;
+console.log(" The applicant’s full name is" , fullname);
+console.log(" The applicant’s current age is" , age);
+console.log(" The value of PI is" , pi);
