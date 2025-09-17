d0cb83490659f5888c8041f87d63ccefe80ac409
Prajukta <prajuktaha@gmail.com>
2025-09-17 21:16:24 +0530

    convert the following data

---
+++ Assignment1/Q4-convertthedata.py
@@ -0,0 +1,17 @@
+//convert 50 into string
+let a = 50;
+let str1 = String(a);
+console.log(str);
+console.log(typeof str);
+
+// Convert ”123” into a number
+ let string = "123";
+ let num = Number(string);
+ console.log(num);
+ console.log(typeof num);
+
+// Convert "" (empty string) into a boolean
+let str = "";
+let boolValue = Boolean(str);
+console.log(boolValue);       
+console.log(typeof boolValue);
