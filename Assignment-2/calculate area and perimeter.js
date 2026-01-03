e8baad90455188abfac5d8817b6e83d36e04776c
Prajukta <prajuktaha@gmail.com>
2026-01-03 22:15:42 +0530

    Implement area and perimeter calculations
    Added calculations for floor area, tile area, and number of tiles needed.

--- Assignment-2/calculate area and perimeter.js
+++ Assignment-2/calculate area and perimeter.js
@@ -1,1 +1,14 @@
 
+let floorLength = prompt("Enter the length of the floor:");
+let floorWidth = prompt("Enter the width of the floor:");
+let tileLength = prompt("Enter the length of a tile:");
+let tileWidth = prompt("Enter the width of a tile:");
+floor length = float(floor length);
+floor width = float(floor width);
+tile length = float(tile length);
+tile width = float(tile width);
+let floorArea = floorLength * floorWidth;
+let tileArea = tileLength * tileWidth;
+let numberOfTiles = float(floorArea / tileArea);
+console.log("Number of tiles needed: " + float(numberOfTiles));
+console.log("Perimeter = " , 2*(a+b));
