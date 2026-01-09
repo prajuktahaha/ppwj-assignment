ecc39c1fbd0974430af50fdd30c2154af921c999
Prajukta <prajuktaha@gmail.com>
2026-01-10 00:02:40 +0530

    salary

--- Assignment-2/Q3 riya salary.js
+++ Assignment-2/Q3 riya salary.js
@@ -2,6 +2,5 @@ let basicSalary = 50000;
 let HRA = 0.2*basicSalary;
 let DA = 0.1*basicSalary;
 let totalSalary = basicSalary + HRA + DA;
-let incomeTax = 0.05*basicSalary;
+let incomeTax = (totalSalary > 100000) ? 0.05*basicSalary : 0;
 console.log("Total income is : " , (totalSalary - incomeTax));
-
