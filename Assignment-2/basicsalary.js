616ec83393226e9acacc16e753e87a4e08b374b7
Prajukta <prajuktaha@gmail.com>
2026-01-09 23:58:46 +0530

    salry of riya

---
+++ Assignment-2/basicsalary.js
@@ -0,0 +1,7 @@
+let basicSalary = 50000;
+let HRA = 0.2*basicSalary;
+let DA = 0.1*basicSalary;
+let totalSalary = basicSalary + HRA + DA;
+let incomeTax = 0.05*basicSalary;
+console.log("Total income is : " , (totalSalary - incomeTax));
+
