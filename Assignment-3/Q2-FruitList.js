3b7920fa26c88ac8f1cc2f9e88cae7baa9ba5b10
Prajukta <prajuktaha@gmail.com>
2026-01-17 21:16:55 +0530

    FruitList

---
+++ Assignment-3/Q2-FruitList.js
@@ -0,0 +1,6 @@
+let FruitList = ["Mango" , "Watermelon" , "Grapes" , "Orange" , "Apple"];
+console.log(FruitList);
+FruitList.sort();
+console.log(FruitList);
+FruitList.reverse();
+console.log(FruitList);
