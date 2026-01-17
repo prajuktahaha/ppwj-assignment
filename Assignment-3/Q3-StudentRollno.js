00abceeb3c2945bf5239c620f373f66a6d5f1ca8
Prajukta <prajuktaha@gmail.com>
2026-01-17 21:42:08 +0530

    Students Roll no

---
+++ Assignment-3/Q3-StudentRollno.js
@@ -0,0 +1,6 @@
+let StudentRollnumber = [12 , 45 , 67 , 89 , 100 , 34 , 23 , 56 , 67];
+console.log(StudentRollnumber);
+let RollNum = StudentRollnumber.indexOf(23);
+console.log(RollNum);
+let Exist = StudentRollnumber.includes(100);
+console.log(Exist);
