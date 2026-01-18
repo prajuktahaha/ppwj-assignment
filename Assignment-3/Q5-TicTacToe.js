8367e069b0681abc46adbe60a28e58f3fb0a1c64
Prajukta <prajuktaha@gmail.com>
2026-01-18 22:05:03 +0530

    print middle elemets of 2d array

---
+++ Assignment-3/Q5-TicTacToe.js
@@ -0,0 +1,13 @@
+let gameBoard = [
+    [1 , 2 , 3],
+    [4 , 5 , 6],
+    [7 , 8 , 9],
+];
+let middleCell = gameBoard[2][1];
+console.log("Middle Cell is : " , middleCell);
+console.log(gameBoard[0]);
+console.log(gameBoard[1]);
+console.log(gameBoard[2]);
+
+//output will be : Middle Cell is : 8
+ad will print entire matrix
