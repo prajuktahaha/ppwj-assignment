a2024f24333ad20797657c72e0543c5636cd83c7
Prajukta <prajuktaha@gmail.com>
2026-01-19 01:40:46 +0530

    create an array of book titles and author

---
+++ Assignment-3/BookStore.js
@@ -0,0 +1,6 @@
+let BookStore = [
+    {title : "Physics Book" , author : "HCVerma"},
+    {title : "Maths Book" , author : "RDSharma"},
+    {title : "Evergreen" , author : "SChand"},
+];
+console.log(BookStore[1]);
